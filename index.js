4f8da758aada40613623844a8b6bd95420f3af50
rachel <fergusonrachel02@gmail.com>
2024-06-07 17:20:24 +0100

    First miity

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log('hello world')
+//variables
+var firstname ='ifeoluwa'
+var Firstname ='sodiq'
+
+console.log(firstname);
+console.log(Firstname);
+
+// with the var you can redeclare and reassign
+var car =  'Toyota'
+console.log(car);
+var car='benz'
+console.log(car);
+car= 'Tesla'
+console.log(car)
+
+// you can only reassign
+let laptop = 'hp'
+laptop = 'macbook'
+
+
+console.log(laptop)
+const gender ='male'
+
+var favoritename = 'pogba'
+var footballclub = 'liverpool'
+var phonenumber = '08106429476'
+ console.log( ' my name is' +    favoritename    + 'my   footballclub'     +   footballclub);
+
+
+ var responsibly = 'sourced corn'
+ var rolled = 'kellogs'
+ var kellogs = 'kellogs'
+  console.log('my favourite' + responsibly + 'my brands' + kellogs + 'my location' + kellogs);
+
+var kilometer
+
+
+ 
